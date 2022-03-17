a3b26627af2cac0ec19880eb37ccd0d069212ded
Fabio da Rosa <fabdarosa@gmail.com>
2022-03-16 21:55:37 -0300

    feat: travel command with multiple choice

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/timongo");
 
 module.exports = mongoose
